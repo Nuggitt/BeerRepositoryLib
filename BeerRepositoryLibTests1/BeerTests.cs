b04713fc20a2f40ff5fca4dba2e847420cd0cc43
Nuggitt <phulip.nielsen@gmail.com>
2024-02-26 20:59:21 +0100

    Add project files.

---
+++ BeerRepositoryLibTests1/BeerTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerRepositoryLib.Tests
+{
+    [TestClass()]
+    public class BeerTests
+    {
+        //Arranging objects
+        Beer beer = new Beer() { Id = 1, Name = "Budweiser", Abv = 5.0 };
+        Beer beer2 = new Beer() { Id = 2, Name = null, Abv = 5.0 };
+        Beer beer3 = new Beer() { Id = 3, Name = "", Abv = 4.6 };
+        Beer beer4 = new Beer() { Id = 4, Name = "Guinness", Abv = 68 };
+        Beer beer5 = new Beer() { Id = 5, Name = "Tuborg Classic", Abv = 4.6 };
+        Beer beer6 = new Beer() { Id = 6, Name = "Bud Light", Abv = -3 };
+        Beer beer7 = new Beer() { Id = 7, Name = "Be", Abv = 4.6 };
+
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            beer.ValidateName();
+            Assert.ThrowsException<ArgumentException>(() => beer2.ValidateName()); //null
+            Assert.ThrowsException<ArgumentException>(() => beer3.ValidateName()); //empty
+            Assert.ThrowsException<ArgumentException>(() => beer7.ValidateName()); //less than 3 characters
+
+        }
+
+        [TestMethod()]
+        public void ValidateAbvTest()
+        {
+            beer.ValidateAbv();
+            Assert.ThrowsException<ArgumentException>(() => beer4.ValidateAbv()); //greater than 67
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer6.ValidateAbv()); //negative
+
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = beer.ToString();
+            Assert.AreEqual("Id: 1, Name: Budweiser, ABV: 5", str); //expected
+
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            beer.Validate();
+            Assert.ThrowsException<ArgumentException>(() => beer2.Validate()); //null
+            Assert.ThrowsException<ArgumentException>(() => beer3.Validate()); //empty
+            Assert.ThrowsException<ArgumentException>(() => beer7.Validate()); //less than 3 characters
+            Assert.ThrowsException<ArgumentException>(() => beer4.Validate()); //greater than 67
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer6.Validate()); //negative
+        }
+    }
+}
---
+++ BeerRepositoryLibTests1/BeersRepositoryTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BeerRepositoryLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerRepositoryLib.Tests
+{
+    [TestClass()]
+    public class BeersRepositoryTests
+    {
+
+        private BeersRepository _beersRepository = new BeersRepository();
+
+
+        [TestMethod()]
+        public void GetBeersTest()
+        {
+            List<Beer> beers = _beersRepository.GetBeers();
+            Assert.AreEqual(6, beers.Count);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            IEnumerable<Beer> beers = _beersRepository.Get(sortBy: "name"); //Sorting by name
+            Assert.AreEqual("Bud Light", beers.First().Name);
+
+            var beersNamedBud = _beersRepository.Get(nameIncludes: "Bud"); //Filtering by name
+            Assert.AreEqual(2, beersNamedBud.Count());
+
+            var beersWithAbv5 = _beersRepository.Get(abv: 5.0); //Filtering by ABV
+            Assert.AreEqual(2, beersWithAbv5.Count());
+
+            var beersSortedByNameDesc = _beersRepository.Get(sortBy: "name_desc"); //Sorting by name descending
+            Assert.AreEqual("Tuborg Classic", beersSortedByNameDesc.First().Name);
+
+            var beersSortedByAbv = _beersRepository.Get(sortBy: "abv"); //Sorting by ABV ascending budlight 3.5
+            Assert.AreEqual("Bud Light", beersSortedByAbv.First().Name);
+
+            var beersSortedByAbvDesc = _beersRepository.Get(sortBy: "abv_desc"); //Sorting by ABV descending budweiser 5.0
+            Assert.AreEqual("Budweiser", beersSortedByAbvDesc.First().Name);
+
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string str = _beersRepository.ToString();
+            Assert.IsTrue(str.Contains("Budweiser"));
+        }
+
+        [TestMethod()]
+        public void AddBeerTest()
+        {
+            _beersRepository.AddBeer(new Beer() { Id = 7, Name = "Becks", Abv = 4.8 });
+            List<Beer> beers = _beersRepository.GetBeers();
+            Assert.AreEqual(7, beers.Count);
+        }
+
+        [TestMethod()]
+        public void RemoveBeerTest()
+        {
+            _beersRepository.RemoveBeer(6); //Removing beer
+            List<Beer> beers = _beersRepository.GetBeers();
+            Assert.AreEqual(5, beers.Count);
+            
+            _beersRepository.RemoveBeer(6); //Removing non existing beer
+            Assert.IsNull(_beersRepository.GetBeerById(6));
+        }
+
+        [TestMethod()]
+        public void GetBeerByIdTest()
+        {
+            Beer? beer = _beersRepository.GetBeerById(1); //Getting beer by id
+            Assert.IsNotNull(beer); 
+            Assert.AreEqual("Budweiser", beer.Name); //Checking if the name is correct
+        }
+
+        [TestMethod()]
+        public void UpdateBeerTest()
+        {
+            _beersRepository.UpdateBeer(1, new Beer() { Id = 1, Name = "Budweiser", Abv = 5.5 }); //Updating beer abv from 5.0 to 5.5
+            Beer? beer = _beersRepository.GetBeerById(1); //Getting beer by id
+            Assert.IsNotNull(beer); //Checking if the beer exists
+            Assert.AreEqual(5.5, beer.Abv); //Checking if the abv is correct
+
+            _beersRepository.UpdateBeer(7, beer); //Updating non existing beer
+            Assert.IsNull(_beersRepository.GetBeerById(7)); //Checking if the beer exists
+        }
+    }
+}
